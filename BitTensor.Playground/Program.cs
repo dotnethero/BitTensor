7ecc8c79da4873c3d089ac7ddfe2195da9eb6d99
dotnethero <andrii.horbokon@gmail.com>
2024-01-27 16:35:06 +0200

    Added playground project

---
+++ BitTensor.Playground/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BitTensor.Playground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
