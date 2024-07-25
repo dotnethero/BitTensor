c25c5e939252066a3c1f3108ce2d77782f69f403
Andrii Horbokon <andrii.horbokon@gmail.com>
2024-07-26 00:13:43 +0300

    CuAllocator as static

--- BitTensor.Native/CuTensor.cs
+++ BitTensor.Native/CuTensor.cs
@@ -1,14 +1,59 @@
 ﻿using BitTensor.Abstractions;
+using BitTensor.CUDA.Abstractions;
 
 namespace BitTensor.CUDA;
-/*
-public class CuTensor : AbstractTensorNode<CuTensor>, IMutableTensor<CuTensor>
+
+public unsafe partial class CuTensor : AbstractTensorNode<CuTensor>, ITensor<CuTensor>, IDeviceArray
 {
+    internal readonly float* Handle;
+
     internal CuTensor(int[] shape) : base(shape)
     {
+        Handle = CuAllocator.Allocate(Size);
+    }
+    
+    internal CuTensor(int[] shape, float[] values) : this(shape)
+    {
+        CuMemory.CopyToDevice(values, Handle, Size);
     }
 
     internal CuTensor(int[] shape, CuTensor[] children, ForwardFunction forward, BackwardFunction backward) : base(shape, children, forward, backward)
     {
+        Handle = CuAllocator.Allocate(Size);
     }
-}*/
+
+    public void CopyToHost(Span<float> destination)
+    {
+        if (destination.Length != Size)
+            throw new ArgumentException($"Destination array size ({destination.Length}) not equal to allocated array size ({Size})");
+
+        CuMemory.CopyToHost(Handle, destination, Size);
+    }
+
+    public void CopyToDevice(ReadOnlySpan<float> source)
+    {
+        if (source.Length != Size)
+            throw new ArgumentException($"Source array size ({source.Length}) not equal to allocated array size ({Size})");
+
+        CuMemory.CopyToDevice(source, Handle, Size);
+    }
+
+    public void Dispose()
+    {
+        CuAllocator.Free(Handle);
+    }
+
+}
+
+public partial class CuTensor
+{
+    public static CuTensor Zero => FromValue(0);
+    public static CuTensor One => FromValue(1);
+
+    public static CuTensor FromArray(int[] shape, float[] values) => 
+        new(shape, values);
+
+    public static CuTensor FromValue(float value) =>
+        FromArray(shape: [], [value]);
+
+}
