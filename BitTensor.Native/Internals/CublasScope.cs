91bfc046a0786e0b3799e45caef2bf4515023e22
Andrii Horbokon <andrii.horbokon@gmail.com>
2024-07-29 22:39:32 +0300

    Interop

---
+++ BitTensor.Native/Internals/CublasScope.cs
@@ -0,0 +1,27 @@
+﻿using BitTensor.CUDA.Interop;
+
+namespace BitTensor.CUDA.Internals;
+
+using static cuBLAS;
+using static cublasStatus_t;
+
+internal readonly unsafe struct CublasScope : IDisposable
+{
+    internal readonly cublasContext* Context;
+
+    public CublasScope()
+    {
+        cublasContext* handle;
+
+        var status = cublasCreate_v2(&handle);
+        if (status != CUBLAS_STATUS_SUCCESS)
+            throw new InvalidOperationException(status.ToString());
+
+        Context = handle;
+    }
+
+    public void Dispose()
+    {
+        cublasDestroy_v2(Context);
+    }
+}
