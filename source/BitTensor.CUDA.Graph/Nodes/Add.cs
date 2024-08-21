3a04a5177b044eaf1a1e9bec357bdea997138fc9
Andrii Horbokon <andrii.horbokon@gmail.com>
2024-08-21 14:01:58 +0300

    Binary operations

---
+++ source/BitTensor.CUDA.Graph/Nodes/Add.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+using BitTensor.Abstractions;
+using BitTensor.CUDA.Plans;
+
+namespace BitTensor.CUDA.Graph.Nodes;
+
+public sealed class Add<T> : AbstractOperation<T> where T : unmanaged, IFloatingPoint<T>
+{
+    internal readonly AbstractNode<T> A;
+    internal readonly AbstractNode<T> B;
+    internal readonly float Alpha;
+    internal readonly float Beta;
+    internal readonly CuTensorTernaryPlan<T> Plan;
+
+    private static Shape GetShape(AbstractTensor a, AbstractTensor b) => Shapes.Broadcast(a.Shape, b.Shape);
+
+    public Add(AbstractNode<T> a, AbstractNode<T> b, float alpha = 1f, float beta = 1f) : base(GetShape(a, b), [a, b])
+    {
+        A = a;
+        B = b;
+        Alpha = alpha;
+        Beta = beta;
+        Plan = Context.cuTENSOR.CreateAddPlan<T>(a.Shape, b.Shape, Shape);
+    }
+
+    public override void EnsureHasUpdatedValue()
+    {
+        A.EnsureHasUpdatedValue();
+        B.EnsureHasUpdatedValue();
+        Plan.Execute(A, B, Tensor, Alpha, Beta, gamma: 0f);
+    }
+
+    public override AbstractNode<T>[] Propagate(AbstractNode<T> gradient)
+    {
+        var adims = Shapes.GetBroadcastedAxis(A.Shape, gradient.Shape);
+        var bdims = Shapes.GetBroadcastedAxis(B.Shape, gradient.Shape);
+        return
+        [
+            new Sum<T>(gradient, axis: adims, scale: Alpha).Reshape(A.Shape),
+            new Sum<T>(gradient, axis: bdims, scale: Beta).Reshape(B.Shape)
+        ];
+    }
+}
---
+++ source/BitTensor.CUDA.Graph/Nodes/Multiply.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+using BitTensor.Abstractions;
+using BitTensor.CUDA.Plans;
+
+namespace BitTensor.CUDA.Graph.Nodes;
+
+public sealed class Multiply<T> : AbstractOperation<T> where T : unmanaged, IFloatingPoint<T>
+{
+    internal readonly AbstractNode<T> A;
+    internal readonly AbstractNode<T> B;
+    internal readonly float Scale;
+    internal readonly CuTensorTernaryPlan<T> Plan;
+
+    private static Shape GetShape(AbstractTensor a, AbstractTensor b) => Shapes.Broadcast(a.Shape, b.Shape);
+
+    public Multiply(AbstractNode<T> a, AbstractNode<T> b, float scale = 1f) : base(GetShape(a, b), [a, b])
+    {
+        A = a;
+        B = b;
+        Scale = scale;
+        Plan = Context.cuTENSOR.CreateMultiplyPlan<T>(a.Shape, b.Shape, Shape);
+    }
+
+    public override void EnsureHasUpdatedValue()
+    {
+        A.EnsureHasUpdatedValue();
+        B.EnsureHasUpdatedValue();
+        Plan.Execute(A, B, Tensor, alpha: Scale, beta: 1f, gamma: 0f);
+    }
+
+    public override AbstractNode<T>[] Propagate(AbstractNode<T> gradient)
+    {
+        var agrad = new Multiply<T>(gradient, B, Scale);
+        var bgrad = new Multiply<T>(gradient, A, Scale);
+        var adims = Shapes.GetBroadcastedAxis(A.Shape, agrad.Shape);
+        var bdims = Shapes.GetBroadcastedAxis(B.Shape, bgrad.Shape);
+        return
+        [
+            new Sum<T>(agrad, axis: adims).Reshape(A.Shape),
+            new Sum<T>(bgrad, axis: bdims).Reshape(B.Shape)
+        ];
+    }
+}
